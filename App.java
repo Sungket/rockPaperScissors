59ca45e713647d67870751b17990395c60ce5bff
Sungket <sungket_rhymes@hotmail.com>
2026-03-04 16:02:19 +0000

    added game files

--- App.java
+++ App.java
@@ -1,7 +1,13 @@
+public class App {
 
-
-class App {
     public static void main(String[] args) {
-        
+
+        Scanner name = new Scanner(System.in);
+        System.out.println("Enter your name");
+
+        User user = new User(name);
+        System.out.println("Welcome " + user.getName + "!");
+
+
     }
 }
---
+++ game.java
@@ -0,0 +1,9 @@
+public class Game {
+
+    private boolean playGame = false;
+    private int score = 0;
+    private String rock;
+    private String paper;
+    private String scissors;
+
+}
